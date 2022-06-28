00c4d7f04374e7609168106046a9197c13a12217
ahmadsultani <88813880+ahmadsultani@users.noreply.github.com>
2022-06-28 19:44:55 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 function helloWorld(){
   return <h1>Hello World</h1>
 }
-
-ReactDOM.render(
-  helloWorld,
-  document.getElementById('app')
-);
